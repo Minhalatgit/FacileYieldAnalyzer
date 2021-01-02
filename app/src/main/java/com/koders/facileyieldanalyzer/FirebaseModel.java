f811a0e73737cb7554a17134404220ea5ba40bc6
Syed Minhal Nadeem <minhalnadeem@gmail.com>
2021-01-02 17:21:20 +0500

    Firebase database integration

---
+++ app/src/main/java/com/koders/facileyieldanalyzer/FirebaseModel.java
@@ -0,0 +1,10 @@
+package com.koders.facileyieldanalyzer;
+
+public class FirebaseModel {
+
+    public double K;
+    public String Moisture;
+    public int N;
+    public int P;
+    public int PH;
+}
--- app/src/main/java/com/koders/facileyieldanalyzer/ResultActivity.java
+++ app/src/main/java/com/koders/facileyieldanalyzer/ResultActivity.java
@@ -1,28 +1,35 @@
 package com.koders.facileyieldanalyzer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.koders.facileyieldanalyzer.databinding.ActivityResultBinding;
 
 public class ResultActivity extends AppCompatActivity {
 
-    Button reScan, exit;
-
+    ActivityResultBinding binding;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_result);
+        databaseReference = FirebaseDatabase.getInstance().getReference("home/");
 
-        reScan = findViewById(R.id.reScan);
-        exit = findViewById(R.id.exit);
-
-        exit.setOnClickListener(new View.OnClickListener() {
+        binding.exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent(ResultActivity.this, LoginActivity.class));
@@ -32,12 +39,27 @@ public class ResultActivity extends AppCompatActivity {
             }
         });
 
-        reScan.setOnClickListener(new View.OnClickListener() {
+        binding.reScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(ResultActivity.this, ExamineActivity.class));
                 finishAffinity();
             }
         });
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                FirebaseModel firebaseModel = (FirebaseModel) snapshot.getValue();
+
+                binding.potassium.setText(String.valueOf(firebaseModel.K));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ResultActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        };
+        databaseReference.addValueEventListener(valueEventListener);
     }
 }
